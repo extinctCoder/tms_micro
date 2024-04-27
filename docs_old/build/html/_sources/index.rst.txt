a3a428fa21714741ca6963ff3067b6cd2f2a7606
Sabbir Ahmed Shourov <extinctCoder@outlook.com>
2024-04-27 13:42:33 +0600

    feat: working on mkdocs as

---
+++ docs_old/build/html/_sources/index.rst.txt
@@ -0,0 +1,20 @@
+.. tms_micro documentation master file, created by
+   sphinx-quickstart on Fri Apr 26 08:29:23 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to tms_micro's documentation!
+=====================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- src/user_service/main.py
+++ src/user_service/main.py
@@ -6,13 +6,39 @@ app = FastAPI()
 
 @app.get("/healthcheck")
 def healthcheck():
+    """
+    Endpoint for health check.
+
+    Returns:
+        Response: A Response object with status code 200 indicating successful health check.
+    """
     return Response(status_code=200)
 
 
 @app.get("/")
 def root_api():
+    """
+    Root API endpoint.
+
+    Returns:
+        dict: A dictionary with a message key indicating the user service.
+    """
     return {"msg": "user_service"}
 
 
 if __name__ == "__main__":
+    """
+    Entry point for running the FastAPI server.
+    Starts the FastAPI server using Uvicorn with the specified host and port.
+
+    Usage:
+        Run this module directly to start the FastAPI server:
+        ```
+        python main.py
+        ```
+
+    Note:
+        The FastAPI server will be accessible at the specified host and port.
+
+    """
     uvicorn.run(app, host="0.0.0.0", port=8000)
