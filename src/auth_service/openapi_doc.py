c58f3e0b27c4a84dc476ecb1b5c57350635b2bb6
Sabbir Ahmed Shourov <extinctCoder@outlook.com>
2024-04-30 17:03:36 +0600

    test: automated api test

--- src/auth_service/openapi_doc.py
+++ src/auth_service/openapi_doc.py
@@ -2,6 +2,7 @@ from main import app, app_contact, app_description, app_title  # type: ignore
 from utils import api_doc  # type: ignore
 
 if __name__ == "__main__":
+    print("Generating Api specification based on OPENAPI_V3.1 for {}".format(app_title))
     api_doc(
         app=app,
         app_title=app_title,
